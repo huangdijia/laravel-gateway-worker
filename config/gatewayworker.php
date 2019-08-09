bdd0eb65190796afe7fa19beecda8e3006027dc3
huangdijia <huangdijia@gmail.com>
2019-08-09 14:27:08 +0800

    add pid and log config

--- config/gatewayworker.php
+++ config/gatewayworker.php
@@ -18,6 +18,6 @@ return [
         'processes'     => env('GATEWAYWORKER_BUSINESSWORKER_PROCESSES', 1),
         'event_handler' => '', // implements \Huangdijia\GatewayWorker\GatewayWorkerEventInterface
     ],
-    'log_file'         => storage_path('workerman.pid'),
-    'pid_file'         => storage_path('workerman.log'),
+    'log_file'         => storage_path('logs/workerman.pid'),
+    'pid_file'         => storage_path('logs/workerman.log'),
 ];
